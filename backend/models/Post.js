2b89572688cd6e7c7c095465197a3da36537a259
Meek Meek <haggins.jameka@gmail.com>
2026-03-24 10:07:28 -0400

    created two models for Post and Users

---
+++ backend/models/Post.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+const Post = new mongoose.model('Post', postSchema)
+
+export default Post
