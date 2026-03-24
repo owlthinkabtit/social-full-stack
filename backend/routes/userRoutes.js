9de0ed4db9f01262c4b896950610de3889a0f8c6
Meek Meek <haggins.jameka@gmail.com>
2026-03-24 10:57:17 -0400

    register route has been created and is looking good.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router() //takes the place of the express.app in server.js
 
 import User from '../models/User.js'
 
+const secret = process.env.JWT_SECRET
+const expiration = '24h'
 router.post('/register', async (req, res) => {
   try {
     const saltRounds = 10
@@ -14,11 +17,20 @@ router.post('/register', async (req, res) => {
       ...req.body,
       password: hashedPassword
     })
-    
-  } catch(err) {
 
+    const payload = { 
+      username: user.username, 
+      email: email, 
+      _id: user._id 
+    }
+    const token = jwt.sign({ data: payload }, secret, {expiresIn: expiration } )
+
+    res.status(201).json({ token, user })
+  } catch (err) {
+    console.log(err.message)
+    res.status(400).json({ message: err.message })
   }
-  
-  
+
+
 })
 
