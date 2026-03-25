6e0d1e24dcbd949d7ec73dc96a65de4f23d4c240
Meek Meek <haggins.jameka@gmail.com>
2026-03-25 09:59:16 -0400

    working to flesh out the registration page. created a form and workin gon wiring it correctly.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
+
 function Register() {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
-      Register
+      <h1>Register Page</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          value={form.username}
+          onChange={handleChange}
+          id="username"
+          name="username"
+          type="text"
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          value={form.email}
+          onChange={handleChange}
+          id="email"
+          name="email"
+          type="email"
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          value={form.password}
+          onChange={handleChange}
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+
+        <button>Register</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
